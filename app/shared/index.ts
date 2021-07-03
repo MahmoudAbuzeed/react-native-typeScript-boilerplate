a6da297b2f6f8edd282a39e08c647dc143320581
Mahmoud Abo-Zeed <mahmoud@Mahmoud.Abozeed>
2021-07-03 16:04:00 +0200

    add boilerplate basic architecture

---
+++ app/shared/index.ts
@@ -0,0 +1,6 @@
+import colors from './colors';
+import fontSize from './fonts/fontSize';
+import fontStyle from './fonts/fontStyle';
+import theme from './theme';
+
+export default {colors, fontStyle, fontSize, theme};
---
+++ app/shared/theme/index.ts
@@ -0,0 +1,6 @@
+import colors from '../colors/index';
+const lightTheme = colors.light;
+const darkTheme = colors.dark;
+
+export {lightTheme, darkTheme};
+export default {lightTheme, darkTheme};
